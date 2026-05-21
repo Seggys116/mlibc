09e244b49a068b8c9f19112c078f13ba1f8f4da0
Seggy116 <zak@zaknobleclarke.com>
2026-05-21 15:37:22 +0100

    Getting somehwere

--- sysdeps/unified/generic/thread.cpp
+++ sysdeps/unified/generic/thread.cpp
@@ -1,7 +1,8 @@
 #include <bits/ensure.h>
 #include <errno.h>
 #include <mlibc/all-sysdeps.hpp>
-#include "../include/mlibc/tcb.hpp"
+#include <mlibc/tcb.hpp>
+#include <mlibc/thread.hpp>
 #include <mlibc/thread-entry.hpp>
 #include <mlibc/debug.hpp>
 #include <stddef.h>
@@ -10,16 +11,6 @@
 #include <unified/syscall.h>
 
 static constexpr uintptr_t kCloneThreadFlag = 0x00010000;
-
-namespace mlibc {
-
-static inline Tcb *get_current_tcb() {
-	uintptr_t ptr;
-	asm volatile ("movq %%fs:0, %0" : "=r"(ptr));
-	return reinterpret_cast<Tcb *>(ptr);
-}
-
-} // namespace mlibc
 
 extern "C" [[gnu::visibility("hidden")]] void __mlibc_enter_thread(void *entry, void *user_arg, Tcb *tcb) {
 	if (!tcb) {
@@ -37,14 +28,11 @@ extern "C" [[gnu::visibility("hidden")]] void __mlibc_enter_thread(void *entry, 
 		mlibc::Sysdeps<FutexWait>::operator()(&tcb->tid, 0, nullptr);
 	}
 
+	__atomic_fetch_or(&tcb->cancelBits, tcbCancelEnableBit, __ATOMIC_RELAXED);
+
 	tcb->invokeThreadFunc(entry, user_arg);
 
-	auto self = mlibc::get_current_tcb();
-
-	__atomic_store_n(&self->didExit, 1, __ATOMIC_RELEASE);
-	mlibc::Sysdeps<FutexWake>::operator()(&self->didExit, false);
-
-	mlibc::Sysdeps<ThreadExit>::operator()();
+	mlibc::thread_exit(tcb->returnValue);
 }
 
 extern "C" [[noreturn, gnu::visibility("hidden")]] void __mlibc_enter_raw_clone(int (*fn)(void *), void *arg, uintptr_t flags) {
